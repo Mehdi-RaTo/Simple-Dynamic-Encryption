eac562dffd50f169d41a61a36adeb57e82bd9175
Mehdi RaTo <Mehdi.RaTo@Gmail.Com>
2024-02-04 17:32:14 +0330

    ...

---
+++ SimpleDynamicEncryption.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public static class SimpleDynamicEncryption
+{
+    public static string Encode(string strInput)
+    {
+        Random rand = new Random();
+        int rndInt = rand.Next(1, 10);
+
+        string result = "";
+        foreach (char c in strInput)
+        {
+            result += (char)(c + rndInt + 2);
+        }
+
+        result += (char)(rndInt + 33);
+
+        return result;
+    }
+
+    public static string Decode(string strInput)
+    {
+        int rndInt = strInput[strInput.Length - 1] - 33;
+
+        strInput = strInput.Substring(0, strInput.Length - 1);
+
+        string result = "";
+        foreach (char c in strInput)
+        {
+            result += (char)(c - rndInt - 2);
+        }
+
+        return result;
+    }
+}
